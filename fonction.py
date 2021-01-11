2f79320f77f03dbdde5b7b4ffc9f46ad1972dd59
gregoire biron <gregoire.biron@hotmail.fr>
2021-01-11 18:47:29 +0100

    ++

--- fonction.py
+++ fonction.py
@@ -1,31 +1,19 @@
-import http.server
+import os
+import smtplib
+import ssl
+from email.message import EmailMessage
 import urllib.request
 import json
 import logging
+import zipfile
+import tarfile
 import pysftp
+import datetime
 
 
 with open('./config.json', 'r') as file:
     data = json.load(file)
 
-def lancer_serv(PORT = 8888, IP=""):
-    PORT = 8888
-    server_address = ("", PORT)
-
-    server = http.server.HTTPServer
-    handler = http.server.CGIHTTPRequestHandler
-    handler.cgi_directories = ["/"]
-    print("Serveur actif sur le port :", PORT)
-
-    httpd = server(server_address, handler)
-    httpd.serve_forever()
-
-    print("mdrr")
-
-def getZip(url):
- urllib.request.urlretrieve(url,"fic_arch.zip")
-
- print("ntm")
 
 #sftp
 def sftpUpload(fileName):
@@ -48,46 +36,71 @@ def sftpDownload(fileName):
 # gestion du fichier
 
 
-logging.basicConfig(filename=data["log"]["filename"], level=logging.DEBUG,
-                    format='%(asctime)s -- %(levelname)s -- %(message)s')
+logging.basicConfig("log.txt", level=logging.DEBUG,format='%(asctime)s -- %(levelname)s -- %(message)s')
+
+class Traitement_fichier:
+    def recupZip(url):
+        to = "sav" + datetime.datetime.now().strftime(data["source"]["dateFormat"])
+        try:
+            urllib.request.urlretrieve(url, to + ".zip")
+            logging.info("Telechargement du fichier %s effectuée avec succes ", data["source"]["zipName"])
+            return to
+        except Exception as e:
+            logging.error(e)
+
+    def Nettoyage(fileName):
+        logging.info("Nettoyage de la dernière sauvegarde")
+        files = {fileName + '.zip', fileName + '.tar.gz', 'dumb.db'}
+        for i in files:
+            if os.path.exists(i):
+                os.remove(i)
 
 
-def clean(fileName):
-    logging.info("Nettoyage de la dernière sauvegarde")
-    files = {fileName + '.zip', fileName + '.tar.gz', 'dumb.db'}
-    for i in files:
-        if os.path.exists(i):
-            os.remove(i)
+    def Verification(zipName):
+        logging.info("verifiation du zip :" + zipName)
+        try:
+            with zipfile.ZipFile(zipName) as zip:
+                if zip.namelist()[0].split(".")[-1] != data["source"]["ext"]:
+                    raise NameError("Mauvais type : le fichier zippé ne contient pas de fichier db")
+                else:
+                    date = (datetime.datetime(*zip.infolist()[0].date_time[0:6]))
+                    logging.info("Le fichier .db a été trouvé")
+                    logging.info("Date du fichier : " + str(date))
+        except Exception as e:
+            logging.error(e)
 
 
-def checkZip(zipName):
-    logging.info("verifiation du zip :" + zipName)
-    try:
-        with zipfile.ZipFile(zipName) as zip:
-            if zip.namelist()[0].split(".")[-1] != data["source"]["ext"]:
-                raise NameError("Mauvais type : le fichier zippé ne contient pas de fichier db")
-            else:
-                date = (datetime.datetime(*zip.infolist()[0].date_time[0:6]))
-                logging.info("Le fichier .db a été trouvé")
-                logging.info("Date du fichier : " + str(date))
-    except Exception as e:
-        logging.error(e)
+    def extractZip(zipName):
+        logging.info("Extraction du fichier :" + zipName)
+        try:
+            with zipfile.ZipFile(zipName, 'r') as zfile:
+                zfile.extractall()
+        except Exception as e:
+            logging.error(e)
 
 
-def extractZip(zipName):
-    logging.info("Extraction du fichier :" + zipName)
-    try:
-        with zipfile.ZipFile(zipName, 'r') as zfile:
-            zfile.extractall()
-    except Exception as e:
-        logging.error(e)
+    def makeTarfile(output_filename, source_dir):
+        logging.info("Compression en tar.gz du fichier :" + output_filename)
+        try:
+            with tarfile.open(output_filename, "w:gz") as tar:
+                tar.add(source_dir, arcname=os.path.basename(source_dir))
+            logging.info("L'archive %s a bien été crée !", output_filename)
+        except Exception as e:
+            logging.error(e)
 
 
-def makeTarfile(output_filename, source_dir):
-    logging.info("Compression en tar.gz du fichier :" + output_filename)
-    try:
-        with tarfile.open(output_filename, "w:gz") as tar:
-            tar.add(source_dir, arcname=os.path.basename(source_dir))
-        logging.info("L'archive %s a bien été crée !", output_filename)
-    except Exception as e:
-        logging.error(e)
+
+class Mail:
+
+    def mail(objet, to, corps):
+        msg = EmailMessage()
+        msg.set_content(corps)
+        msg["Subject"] = objet
+        msg["From"] = data["mail"]["login"]
+        msg["To"] = to
+
+        context = ssl.create_default_context()
+        with smtplib.SMTP(data["mail"]["hostname"],data["mail"]["port"]) as smtp:
+            smtp.starttls(context=context)
+            smtp.login(data["mail"]["login"], data["mail"]["password"])
+            smtp.send_message(msg)
--- main.py
+++ main.py
@@ -2,5 +2,41 @@ from fonction import *
 
 
 url = "http://127.0.0.1:8888/"
+
 if __name__ == '__main__':
-    getZip(url)
+    try:
+
+        # On définit le nom de la sauvegarde
+        fileName = "sav" + datetime.datetime.now().strftime(data["source"]["dateFormat"])
+
+        # on definit le nom du zip de sauvegarde
+        zipName = fileName + ".zip"
+
+        # on definit le nom du tar.gz de sauvegarde
+        tarName = fileName + ".tar.gz"
+
+        # on nettoye les fichiers temporaires qui pourrait creer des conflits
+        Traitement_fichier.Nettoyage(fileName)
+
+        # on va chercher le zip sur le server web
+        Traitement_fichier.recupZip(data["source"]["url"])
+
+        # on verifie le contenu du zip
+        Traitement_fichier.Verification(zipName)
+
+        # on dézip le fichier
+        Traitement_fichier.extractZip(zipName)
+
+        # on le recompresse au format Tar.ge
+        Traitement_fichier.makeTarfile(tarName, data["source"]["name"])
+
+        # on l'envoie sur le serveur distant
+        sftpUpload(tarName)
+
+        # on envoie un mail en cas de réussite
+        Mail.mail("réussite", data["mail"]["to"], "corps")
+
+    except Exception as e:
+        print(e)
+        logging.critical(e)
+        Mail.mail("echec", data["mail"]["to"], e)
